4d7359aceea9b57c5bbb4eee1f5049bea5305c3a
Zartaich <zartaich@gmail.com>
2019-03-20 01:50:08 +0300

    FEAT: breckets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const arr = str.split('');
+
+  if (arr.length % 2 !== 0) {
+    return false;
+  }
+
+  const stack = [];
+
+  for (let i = 0; arr.length > i; i++) {
+    for (let y = 0; bracketsConfig.length > y; y++) {
+      if (bracketsConfig[y][1] === arr[i] && bracketsConfig[y][0] === stack[stack.length - 1]) {
+        stack.pop();
+      } else if (arr[i] === bracketsConfig[y][0]) {
+        stack.push(arr[i]);
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
+
